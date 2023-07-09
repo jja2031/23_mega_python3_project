3d4c0c80e6084287c6e00a9d144c0dac23d31815
jja2031 <ganghuig45@gmail.com>
2023-07-09 15:50:27 +0900

    :memo:update

--- 김강희/data_project/import turtle as t.py
+++ 김강희/data_project/import turtle as t.py
@@ -1,19 +1,13 @@
-# Dino Run
-# 2D game inspired by Google Chrome's dinosaur game
-
-# Original Author: Robin Rezwan
-# GitHub: http://github.com/robinrezwan
-
 import pygame
 from pygame.locals import *
 import os
 import sys
 import random
 
-# set the game window at the center of the display
+
 os.environ["SDL_VIDEO_CENTERED"] = "1"
 
-# defining some global variables
+
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
@@ -23,74 +17,56 @@ GROUND_HEIGHT = SCREEN_HEIGHT - 70
 
 PLAY_GAME = True
 
-# initialize pygame and create window
+
 pygame.init()
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Run")
 
 clock = pygame.time.Clock()
 
-# load audio files
+
 jump_sound = pygame.mixer.Sound("sound/jump.ogg")
 score_sound = pygame.mixer.Sound("sound/score.ogg")
 game_over_sound = pygame.mixer.Sound("sound/game_over.ogg")
 
 
-# function for drawing text on the screen
 def draw_text(text, font_name, size, text_color, position_x, position_y, position):
-    font = pygame.font.Font(font_name, size)  # loads font
+    font = pygame.font.Font(font_name, size)
 
-    text_plane = font.render(
-        text, True, text_color
-    )  # renders given text in the selected font
+    text_plane = font.render(text, True, text_color)
     text_rect = text_plane.get_rect()
 
-    # setting text position
     if position == "midtop":
         text_rect.midtop = (int(position_x), int(position_y))
     elif position == "topright":
         text_rect.topright = (int(position_x), int(position_y))
 
-    window.blit(text_plane, text_rect)  # draws the rendered text on the screen
+    window.blit(text_plane, text_rect)
 
 
-# function for loading single image file
 def load_image(path, size_x=0, size_y=0):
-    image = pygame.image.load(
-        path
-    ).convert_alpha()  # loads image file and converts it into pixels
+    image = pygame.image.load(path).convert_alpha()
 
     if size_x > 0 and size_y > 0:
-        image = pygame.transform.scale(
-            image, (size_x, size_y)
-        )  # resizing the image to the given size
+        image = pygame.transform.scale(image, (size_x, size_y))
 
     return image, image.get_rect()
 
 
-# function for loading multiple image files in a list
 def load_sprites(image_path, image_name_prefix, number_of_image, size_x=0, size_y=0):
-    images = []  # declaring list to store the images
+    images = []
 
     for i in range(0, number_of_image):
-        path = (
-            image_path + image_name_prefix + str(i) + ".png"
-        )  # creating the path string
-        image = pygame.image.load(
-            path
-        ).convert_alpha()  # loads image file and converts it into pixels
-
+        path = image_path + image_name_prefix + str(i) + ".png"
+        image = pygame.image.load(path).convert_alpha()
         if size_x > 0 and size_y > 0:
-            image = pygame.transform.scale(
-                image, (size_x, size_y)
-            )  # resizing the image to the given size
+            image = pygame.transform.scale(image, (size_x, size_y))
 
         images.append(image)
 
     return images
 
 
-# class for creating and moving single background
 class Background:
     def __init__(self, image_path, speed=10):
         self.image0, self.rect0 = load_image(image_path, 1280, 720)
@@ -118,7 +94,6 @@ class Background:
             self.rect1.left = self.rect0.right
 
 
-# class for creating and moving single multiple backgrounds using the Background class
 class AllBackgrounds:
     def __init__(self, game_speed):
         self.background_0 = Background("image/background/bg_0.png", game_speed)
@@ -145,7 +120,6 @@ class AllBackgrounds:
         self.background_0.update()
 
 
-# class for creating and moving obstacle cactus
 class Cactus:
     def __init__(self, speed=10):
         self.cactus_images = load_sprites("image/cactus/", "cactus_", 5, 160, 160)
@@ -216,7 +190,6 @@ class Cactus:
             self.cactus_image_1 = self.cactus_images[temp_index]
 
 
-# class for creating and moving our dino buddy
 class Dino:
     def __init__(self):
         self.idle_images = load_sprites("image/dino/", "idle_", 10, 220, 153)
@@ -229,22 +202,20 @@ class Dino:
         self.rect.left = 70
 
         self.jump_limit = GROUND_HEIGHT - 290
-        self.jump_speed = 50  # starting speed of the jump
-        self.gravity_up = 4  # change rate when jumping up
-        self.gravity_down = 2  # change rate when falling down
+        self.jump_speed = 50
+        self.gravity_up = 4
+        self.gravity_down = 2
 
-        # these indexes cycle through the images of the sprites, make the dino look moving
         self.idle_index = 0
         self.running_index = 0
         self.jumping_index = 0
 
-        # these booleans determine which images should be shown
         self.idle = True
         self.running = False
         self.jumping = False
         self.falling = False
 
-        self.call_count = 0  # this variable is used to determine how often a task in a function should be done
+        self.call_count = 0
 
     def check_collision(self, all_cactus):
         if self.running:
@@ -324,7 +295,6 @@ class Dino:
         self.call_count = self.call_count + 1
 
 
-# class for counting and drawing score on the screen and saving high score on a file
 class Score:
     def __init__(self):
         self.high_score_image, self.rect_high = load_image(
@@ -384,7 +354,6 @@ class Score:
         )
 
     def load(self):
-        # load high score
         try:
             with open("high_score.txt", "r") as file:
                 self.high_score = int(file.read())
@@ -392,13 +361,11 @@ class Score:
             self.high_score = 0
 
     def save(self):
-        # save high score
         if self.high_score_achieved:
             with open("high_score.txt", "w") as file:
                 file.write(str(self.high_score))
 
 
-# class for showing game over screen
 class GameOver:
     def __init__(self):
         self.replay_image, self.rect = load_image(
@@ -420,14 +387,12 @@ class GameOver:
         window.blit(self.replay_image, self.rect)
 
 
-# main game function
 def start_game():
-    # declaring necessary variables and creating objects of the classes
     run = True
     play_again = False
     game_over = False
 
-    game_speed = 15  # the speed the number of pixels the game moves
+    game_speed = 15
 
     backgrounds = AllBackgrounds(game_speed)
     cactus = Cactus(game_speed)
@@ -435,21 +400,18 @@ def start_game():
     score = Score()
     game_over_screen = GameOver()
 
-    # main game loop, this will run continuously and draw everything on the screen
     while run:
-        clock.tick(FPS)  # limiting frames per second to run loop at the right speed
+        clock.tick(FPS)
 
-        # handling input events
         for event in pygame.event.get():
             if event.type == QUIT:
-                pygame.quit()  # quits pygame
-                sys.exit()  # exits the program
+                pygame.quit()
+                sys.exit()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                mx, my = pygame.mouse.get_pos()  # gets mouse click co-ordinates
+                mx, my = pygame.mouse.get_pos()
 
                 if game_over:
-                    # checks if the play again button is clicked
                     if (
                         game_over_screen.rect.left < mx < game_over_screen.rect.right
                         and game_over_screen.rect.top
@@ -459,7 +421,7 @@ def start_game():
                         play_again = True
                         run = False
 
-        key = pygame.key.get_pressed()  # gets pressed key
+        key = pygame.key.get_pressed()
 
         if key[K_SPACE] or key[K_UP]:
             if game_over:
@@ -473,7 +435,6 @@ def start_game():
                 if dino.idle:
                     dino.idle = False
 
-        # calling draw functions to draw all the elements on the screen
         backgrounds.draw()
         cactus.draw()
         dino.draw()
@@ -489,7 +450,6 @@ def start_game():
                 backgrounds.update()
                 cactus.update()
 
-                # increasing game speed over time
                 if score.score % 120 == 0:
                     game_speed += 0.5
                     backgrounds.update_speed(game_speed)
@@ -498,18 +458,16 @@ def start_game():
 
             dino.update()
 
-            # calling function to check collision
             if dino.check_collision(cactus.get_cactus()):
                 game_over = True
                 game_over_screen.draw()
                 game_over_sound.play()
                 score.save()
 
-        pygame.display.flip()  # clears the display before running the loop again
+        pygame.display.flip()
 
-    return play_again  # returns true after game over, if the player wants to play again
+    return play_again
 
 
-# this loop keeps calling the main game function as long as the player wants to continue
 while PLAY_GAME:
     PLAY_GAME = start_game()
