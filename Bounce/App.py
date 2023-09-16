e0c6e7d26c8a2d3013b61859b886bfed2bef1902
GamerChris0837 <jjangga4227@gmail.com>
2023-09-16 15:57:42 +0900

    :memo:

--- Bounce/App.py
+++ Bounce/App.py
@@ -10,22 +10,31 @@ color = (80, 188, 223)
 r = 20
 xspeed = 0.05
 yspeed = 0.05
+img = pygame.image.load('Bounce\pngwingcom.png')
+pygame.display.set_icon(img)
 
 pygame.display.set_caption("Bounce")
 
 x = screen_width/2
 y = screen_height/2
 
-(h, s, v) = (90/360.0, 1.0, 1.0)
+(h, l, s) = (0.0, 127.5, -1.007905138339921)
 
 running = True
 while running:
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x_pos = int(pygame.mouse.get_pos()[0])
+            y_pos = int(pygame.mouse.get_pos()[1]) 
+            x = x_pos
+            y = y_pos
 
     x += xspeed
     y += yspeed
+    
+    h += 0.00005
 
     if x < r or x > screen_width - r:
         xspeed *= -1
@@ -36,9 +45,7 @@ while running:
     h += 1
     
     screen.fill(color)
-    pygame.draw.circle(screen, colorsys.hsv_to_rgb(h, s, v), (x, y), r)
+    pygame.draw.circle(screen, colorsys.hls_to_rgb(h, l, s), (x, y), r)
     pygame.display.flip()
 
-
-#pygame 종료s
 pygame.quit()
